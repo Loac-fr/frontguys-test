9baa1b7a496c4268a952cac4576fe4f9266dff2d
Loic <90761328+LoicSelency@users.noreply.github.com>
2024-12-12 18:14:33 +0100

    initial commit

---
+++ frontguys-test/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex align-top">
+    <div class="md:w-1/2">
+      <h1 class="text-xxl md:text-[20vw] text-neutral-900">Register now</h1>
+    </div>
+    <div class="bg-green-200 md:w-1/2">
+      <FormRegister/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
